6be27298ca5300b9f11373fc1ca7eca31ebc2b97
Uzair Jawaid <46062831+UzairJ99@users.noreply.github.com>
2019-11-30 17:56:02 -0500

    Solutin to Two Sum II using a hashmap and enumerate()

--- twoSumII.py
+++ twoSumII.py
@@ -1,22 +1,13 @@
 class Solution:
     def twoSum(self, numbers, target):
-        sums = {}
-        output = []
-        
-        #iterate through each element and perform (target - element)
-        for element in numbers:
-            #hashmap that element to it's difference in a dictionary (element: target-element)
-            sums[element] = target - element
-
-        #iterate through the list
-        for key in numbers:
-            #if the value exists both in the sums and nums dictionary, get the index
-            if sums.get(key) in numbers:
-                output.append(numbers.index(key)+1)
-                numbers.remove(key) #remove the element to avoid duplicate indices
-                output.append(numbers.index(sums.get(key))+2) #increment index after above deletion
-                break #ignore duplicate pairings
+        keys = {}
+        for index, value in enumerate(numbers):
+            #check if the difference value is already in the dictionary
+            if (target - value) in keys: 
+                #if the difference already exists in the dictionary, we've found a pair
+                break
             else:
-                continue
-        
-        return output       
+                #add the value to the dictionary; non-zero indexed
+                keys[value] = index + 1
+        #sort the indices
+        return sorted([keys[target - value], index + 1])
