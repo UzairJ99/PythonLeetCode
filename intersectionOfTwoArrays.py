d814f067a846c18bf3108fbb53bc62428b4969e0
Uzair Jawaid <46062831+UzairJ99@users.noreply.github.com>
2019-11-30 14:55:29 -0500

    Solution to Intersection of Two Arrays using Binary Search

---
+++ intersectionOfTwoArrays.py
@@ -0,0 +1,40 @@
+class Solution:
+    def intersection(self, nums1, nums2):
+        output = []
+        nums1 = sorted(nums1)
+        nums2 = sorted(nums2)
+        #find shorter list
+        if len(nums1) >= len(nums2):
+            largerList = nums1
+            shorterList = nums2
+        else:
+            largerList = nums2
+            shorterList = nums1
+
+        #binarySearch function
+        def binarySearch(arr, val):
+            hi, lo = len(arr)-1, 0
+            mid = (hi + lo) // 2
+
+            while lo <= hi:
+                mid = (hi + lo) // 2
+                if arr[mid] == val:
+                    print(val)
+                    return val 
+                elif val > arr[mid]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+            #not found
+            return -1
+
+        #iterate binary search for each element in shorter list on the longer list
+        for element in shorterList:
+            foundElement = binarySearch(largerList, element)
+            if foundElement != -1:
+                output.append(foundElement)
+            else:
+                continue
+
+        #remove duplicates
+        return list(set(output))
