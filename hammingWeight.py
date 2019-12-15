bb92173801dec88c6b3269f7efc7057b3b3afd95
Uzair Jawaid <46062831+UzairJ99@users.noreply.github.com>
2019-12-15 13:49:04 -0500

    Solution to Hamming Weight using XOR bit manipulation in O(n) time and O(1) space

--- hammingWeight.py
+++ hammingWeight.py
@@ -2,7 +2,7 @@ class Solution:
     def hammingWeight(self, n):
         count = 0
         #XOR each bit with 0
-        for bit in str(n):
+        for bit in str(bin(n))[2:]:
             #1^1= 1 which will add to the count of 1s
             count += (int(bit)^0)
             
